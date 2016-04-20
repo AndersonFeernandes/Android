7b0d6ec3d54b8339c77c312c278f3d4e7fe56460
AndersonFeernandes <andersonlcfernandes@gmail.com>
2016-04-20 01:04:36 -0300

    Android Studio Agenda
    Projeto criado para aprendizagem no Android Studio 2.0

---
+++ Agenda/app/src/main/java/br/com/fernandesworkgroup/agenda/modelo/Aluno.java
@@ -0,0 +1,68 @@
+package br.com.fernandesworkgroup.agenda.modelo;
+
+import java.io.Serializable;
+
+/**
+ * Created by PC-CASA on 19/04/2016.
+ */
+public class Aluno implements Serializable {
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String site;
+    private double nota;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return getId() + " - " + getNome();
+    }
+}
